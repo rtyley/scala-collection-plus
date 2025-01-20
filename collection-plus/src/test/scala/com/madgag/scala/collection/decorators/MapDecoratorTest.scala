1e0d758af497ed910dde344e45f64296826a8297
Roberto Tyley <roberto.tyley@gmail.com>
2025-01-20 13:34:32 +0000

    Add alternative for filterKeys
    https://github.com/search?q=org%3Aguardian+filterKeys+language%3AScala&type=code&l=Scala

--- collection-plus/src/test/scala/com/madgag/scala/collection/decorators/MapDecoratorTest.scala
+++ collection-plus/src/test/scala/com/madgag/scala/collection/decorators/MapDecoratorTest.scala
@@ -9,6 +9,7 @@ import scala.collection.immutable.SortedMap
 class MapDecoratorTest extends AnyFlatSpec with should.Matchers {
 
   val m = Map("foo" -> 5, "bar" -> 10)
+  val sm = SortedMap("foo" -> "apple", "" -> "b", "bar" -> "tree", "boo" -> "is", "aaa" -> "x")
 
   def check_mapV_matchesOfficialImplementationFor[K,V,U](m: Map[K,V])(f: V => U): Assertion = {
     val distinctResultsForMappingValuesByDifferentMethods = Set[Map[K,U]](
@@ -16,6 +17,14 @@ class MapDecoratorTest extends AnyFlatSpec with should.Matchers {
       m.view.mapValues(f).toMap,
       // m.mapValues(f) - deprecated in Scala 2.13, now returns MapView[K,U] rather than Map[K,U]
       m.mapV(f),
+    )
+    distinctResultsForMappingValuesByDifferentMethods should have size 1
+  }
+
+  def check_filterK_matchesOfficialImplementationFor[K,V](m: Map[K,V])(p: K => Boolean): Assertion = {
+    val distinctResultsForMappingValuesByDifferentMethods = Set[Map[K,V]](
+      m.view.filterKeys(p).toMap,
+      m.filterK(p),
     )
     distinctResultsForMappingValuesByDifferentMethods should have size 1
   }
@@ -28,5 +37,14 @@ class MapDecoratorTest extends AnyFlatSpec with should.Matchers {
     check_mapV_matchesOfficialImplementationFor(Map("foo" -> 6, "bar" -> 11))(_ + 1)
     check_mapV_matchesOfficialImplementationFor(Map("foo" -> "apple", "bar" -> "tree"))(_.length)
     check_mapV_matchesOfficialImplementationFor(SortedMap("foo" -> "apple", "bar" -> "tree", "boo" -> "is"))(_.length)
+  }
+
+  "filterV" should "create sensible results for filtering the keys of a map" in {
+    m.filterK(_.contains("o")) shouldBe Map("foo" -> 5)
+  }
+
+  it should "create results identical to those produced by the official Scala library implementations" in {
+    check_filterK_matchesOfficialImplementationFor(Map("foo" -> 6, "bar" -> 11))(_.contains("a"))
+    check_filterK_matchesOfficialImplementationFor(SortedMap("foo" -> "apple", "" -> "b", "bar" -> "tree", "boo" -> "is"))(_.nonEmpty)
   }
 }
